032e26874fcddf2bede1dd5f7b1d58e6f88dca94
Rich Daley <rich@fishpercolator.co.uk>
2018-04-22 19:13:29 +0100

    Add the frontend domain to the cors settings

--- autheg-backend/config/initializers/cors.rb
+++ autheg-backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000', 'autheg.herokuapp.com'
 
     resource '*',
       headers: :any,
